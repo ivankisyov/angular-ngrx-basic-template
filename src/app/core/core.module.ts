c17cbc79f50702a06b3c9669281eaaff1b0ab002
Ivan Kisyov <dev.vanks@gmail.com>
2024-02-17 13:13:32 +0200

    App init, create core module and header and footer components

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule {
+  constructor (@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error(
+        'CoreModule is already loaded. Import it in the AppModule only');
+    }
+  }
+}
